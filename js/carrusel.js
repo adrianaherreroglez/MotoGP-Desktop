6f1df0f7448ba500b435998994c6656f35ad907d
uo287543 <uo287543@uniovi.es>
2025-12-20 15:59:23 +0100

    probar otra vez

--- js/carrusel.js
+++ js/carrusel.js
@@ -16,10 +16,9 @@ class Carrusel {
         this.#maximo = 4; // índices 0-4
         this.#fotosJSON = { fotos: [] };
         this.#imagenElemento = null;
-        this.#getFotografias();
     }
 
-    #getFotografias() {
+    getFotografias() {
         const url = "https://www.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
 
         $.ajax({
@@ -32,8 +31,8 @@ class Carrusel {
                 format: "json"
             },
             success: function (datos) {
-                this.#procesarJSONFotografias(datos);
-                this.#mostrarFotografias();
+                this.procesarJSONFotografias(datos);
+                this.mostrarFotografias();
             }.bind(this),
             error: function () {
                 const h2 = document.createElement("h2");
@@ -43,10 +42,10 @@ class Carrusel {
         });
     }
 
-    #procesarJSONFotografias(datos) {
+    procesarJSONFotografias(datos) {
         const fotos = [];
 
-        for (let i = 0; i < datos.items.length && fotos.length <= this.#maximo; i++) {
+        for (let i = 0; i < datos.items.length && fotos.length <= this.#maximo ; i++) {
             const url = datos.items[i].media.m.replace("_m.", "_z.");
             if (!fotos.includes(url)) fotos.push(url);
         }
@@ -55,11 +54,9 @@ class Carrusel {
         this.#actual = 0;
     }
 
-    #mostrarFotografias() {
-        if (!main) {
-            main = document.createElement("main");
-            document.body.appendChild(main);
-        }
+    mostrarFotografias() {
+        const main = document.querySelector("main");
+        if (!main) return;
 
         const articulo = document.createElement("article");
 
@@ -76,11 +73,12 @@ class Carrusel {
 
         this.#imagenElemento = imagen;
 
-        setInterval(this.#cambiarFotografia.bind(this), 3000);
+   
+        setInterval(this.cambiarFotografia.bind(this), 3000);
+
     }
 
-
-    #cambiarFotografia() {
+    cambiarFotografia() {
         this.#actual++;
         if (this.#actual > this.#maximo) this.#actual = 0;
 
